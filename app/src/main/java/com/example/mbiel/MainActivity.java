7e200fa057a7f45e8d94a0d82def40368b236c2e
Tenebresus <tyler.teppema20@gmail.com>
2020-12-11 13:36:41 +0100

    request

--- app/src/main/java/com/example/mbiel/MainActivity.java
+++ app/src/main/java/com/example/mbiel/MainActivity.java
@@ -1,15 +1,45 @@
 package com.example.mbiel;
 
 import android.os.Bundle;
-import org.opencv.android.*;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    final MediaType MEDIA_TYPE_JPG = MediaType.parse("image/jpg");
+    final File file = new File("messi.jpg");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //rest comment
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody req = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("image","messi.jpg", RequestBody.create(MEDIA_TYPE_JPG, file))
+                .build();
+        Request request = new Request.Builder()
+                .url("http://86.83.86.194:5000/test")
+                .post(req)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
