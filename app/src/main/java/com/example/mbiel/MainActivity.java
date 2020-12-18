e6a8d84c59eef273cb955339e2d9805ed980d74f
Tenebresus <tyler.teppema20@gmail.com>
2020-12-18 14:36:05 +0100

    fixed openGallery and openCamera

--- app/src/main/java/com/example/mbiel/MainActivity.java
+++ app/src/main/java/com/example/mbiel/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.mbiel;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.media.Image;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -17,17 +16,20 @@ public class MainActivity extends AppCompatActivity{
     ImageView imageView;
     Uri imageUri;
     private static final int PICK_IMAGE = 100;
+    private static final int TAKE_PICTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView)findViewById(R.id.imageView);
+
         // Als je op de button klikt opent hij de camera
         final Button takePictureButton = findViewById(R.id.takePictureButton);
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+                openCamera();
             }
         });
 
@@ -44,6 +46,10 @@ public class MainActivity extends AppCompatActivity{
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
     }
+    private void openCamera(){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, TAKE_PICTURE);
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
@@ -51,17 +57,10 @@ public class MainActivity extends AppCompatActivity{
             imageUri = data.getData();
             imageView.setImageURI(imageUri);
         }
-    }
-
-    // Code start IMAGE_CAPTURE intent
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
+        if(requestCode == TAKE_PICTURE && resultCode == RESULT_OK){
+            Bitmap photo = (Bitmap)data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+            imageView.setVisibility(View.VISIBLE);
         }
     }
 }
