e0bddd5a2fefca15104f586565347100156be1ee
Tenebresus <tyler.teppema20@gmail.com>
2020-12-18 14:15:01 +0100

    added gallery button and functionality

--- app/src/main/java/com/example/mbiel/MainActivity.java
+++ app/src/main/java/com/example/mbiel/MainActivity.java
@@ -2,28 +2,58 @@ package com.example.mbiel;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.net.Uri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
+
+    ImageView imageView;
+    Uri imageUri;
+    private static final int PICK_IMAGE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        // Als je op de button klikt opent hij de camera
+        final Button takePictureButton = findViewById(R.id.takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
         });
+
+        // Als je op de button klikt pakt hij een image van je gallerij
+        final Button uploadPictureButton = findViewById(R.id.uploadPictureButton);
+        uploadPictureButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
     }
 
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+
+    // Code start IMAGE_CAPTURE intent
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private void dispatchTakePictureIntent() {
