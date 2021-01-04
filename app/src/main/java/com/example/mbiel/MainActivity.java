3fb814155766087a7f6552fed94006a9e6df96f1
Nickoetje <55237227+Nickoetje@users.noreply.github.com>
2021-01-04 09:38:52 +0100

    uploading image fixed

--- app/src/main/java/com/example/mbiel/MainActivity.java
+++ app/src/main/java/com/example/mbiel/MainActivity.java
@@ -1,11 +1,30 @@
 package com.example.mbiel;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Base64;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -13,25 +32,22 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
-import okhttp3.FormBody;
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity{
 
     final MediaType MEDIA_TYPE_JPG = MediaType.parse("image/jpg");
-    File file;
+    private static final int STORAGE_PERMISSION_CODE = 123;
     public static final int PICK_IMAGE = 1;
+    Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestStoragePermission();
 
         Intent intent = new Intent();
         intent.setType("image/*");
@@ -43,90 +59,74 @@ public class MainActivity extends AppCompatActivity{
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE) {
             if (resultCode == RESULT_OK) {
-
-                Uri uri = data.getData();
-                InputStream iStream = null;
+                final Uri imageUri = data.getData();
+                final InputStream imageStream;
                 try {
-                   iStream =   getContentResolver().openInputStream(uri);
-                    byte[] inputData = getBytes(iStream);
-                    createFile(inputData);
-
-                } catch (IOException e) {
+                    imageStream = getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    String encodedImage = encodeImage(selectedImage);
+                    volleyPost(encodedImage);
+                } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
 
 
-//                System.out.println(data.getDataString());
-//                if(file.exists()){
-//                    System.out.println(file);
-//
-//                    new Thread(new Runnable() {
-//                        public void run(){
-//                            String a = null;
-//                            try {
-//                                a = runA("http://86.83.86.194:5000/test");
-//                            } catch (IOException e) {
-//                                e.printStackTrace();
-//                            }
-//                            System.out.println(a);
-//                        }
-//                    }).start();
-//                }else{
-//                    System.out.println("cant find file");
-//                }
+
 
             }
         }
     }
 
-    OkHttpClient client = new OkHttpClient();
+    public void volleyPost(String dat){
+        String postUrl = "http://192.168.178.230:5000/test";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-    public byte[] getBytes(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-        int bufferSize = 1024;
-        byte[] buffer = new byte[bufferSize];
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("data", dat);
 
-        int len = 0;
-        while ((len = inputStream.read(buffer)) != -1) {
-            byteBuffer.write(buffer, 0, len);
-        }
-        System.out.println(byteBuffer.toByteArray().length);
-        return byteBuffer.toByteArray();
-    }
-
-    String runA(String url) throws IOException {
-        RequestBody req = new MultipartBody.Builder().setType(MultipartBody.FORM)
-                .addFormDataPart("messi","messi.jpg", RequestBody.create(MEDIA_TYPE_JPG, file)).build();
-        Request request = new Request.Builder()
-                .url(url)
-                .post(req)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
-    }
-    private void createFile(byte[] fileData) {
-                try {
-                    //Create directory..
-            File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/Images");
-            File dir = new File(root + File.separator);
-            if (!dir.exists()) dir.mkdir();
-
-            //Create file..
-            file = new File(root + File.separator + "image.jpg");
-            file.createNewFile();
-
-            FileOutputStream out = new FileOutputStream(file);
-            out.write(fileData);
-            out.close();
-
-        }
-        catch (FileNotFoundException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+
     }
+    private String encodeImage(Bitmap bm)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        byte[] b = baos.toByteArray();
+        String encImage = Base64.encodeToString(b, Base64.DEFAULT);
+
+        return encImage;
+    }
+
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+    }
+
+
+
 }
